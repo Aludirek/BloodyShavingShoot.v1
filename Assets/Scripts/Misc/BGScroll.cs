be5bb341310688c68a3c011976a1aac0e06a0b2f
Aludirek <43136919+Aludirek@users.noreply.github.com>
2020-07-09 01:10:06 +0200

    - added new movable bg with effects

---
+++ Assets/Scripts/Misc/BGScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = 0.3f;
+
+    private Vector2 offset;
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!GetComponent<MeshRenderer>())
+            return;
+        float scrollAmnt = Time.time * scrollSpeed;
+        offset.x = 0;
+        offset.y = scrollAmnt;
+        GetComponent<MeshRenderer>().material.SetTextureOffset("_MainTex", offset);
+    }
+}
